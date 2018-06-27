022da24dcfae0d6dc81859afdad0deba876fc9d7
Juliana Kang <julianajlk@gmail.com>
2018-06-27 17:54:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,6 +23,8 @@ end
 
 def printer(array)
   batch_badge_creator(array).each do |badge|
-  puts badge
-end
+    puts badge
+  end
+  assign_rooms(array).each do |assignment|
+    puts assignment
 end
