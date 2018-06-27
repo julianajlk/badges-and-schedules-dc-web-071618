caa05eb26a73ee35b45f57ea169ee0fd0d2734b7
Juliana Kang <julianajlk@gmail.com>
2018-06-27 17:03:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = []
+  array.each do |name|
+    badges.push("Hello, my name is #{name}.")
+  end
+  return badges
+end
+
+def assign_rooms(array)
+  rooms = []
+  room_number = 1
+  array.each do |name|
+    rooms.push("Hello, #{name}! You'll be assigned to room #{room_number}!")
+    room_number += 1
+  end
+  return rooms
+end
